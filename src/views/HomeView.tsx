af1c0be2f3c3a588f7bf191a3e061ed7e57d81e5
Daniele Germani <daniele.germani.85@gmail.com>
2026-03-23 16:41:29 +0100

    feat: setup develop

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -4,13 +4,13 @@ export default function HomeView() {
   return (
     <div className="home-container">
       <img
-        src="https://www.builder.io/m/design-systems/logo"
+        src="https://dka575ofm4ao0.cloudfront.net/pages-transactional_logos/retina/786927/Screenshot_2023-06-07_at_3.00.51_PM.png"
         alt="Builder.io"
         className="logo-left"
       />
       <h1 className="home-title">Pildora Builder.io</h1>
       <img
-        src="https://www.nttdata.com/content/dam/nttdata-logo/logo.svg"
+        src="https://www.drupal.org/files/styles/grid-4-2x/public/Logo%20Global%20NTT%20DATA%20Future%20Blue%20RGB.png?itok=J9FK9FNw"
         alt="NTT Data"
         className="logo-right"
       />
