96bd08a5a703638365a621f72f0d2235959e5643
Daniele Germani <daniele.germani.85@gmail.com>
2026-04-08 11:54:17 +0200

    feat: remove pokemon view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomeView from './views/HomeView'
-import PokemonView from './views/PokemonView'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeView />} />
-        <Route path="/pokedex" element={<PokemonView />} />
       </Routes>
     </BrowserRouter>
   )
